06d9a7324b823f1884137b43dd599b7b4cef8197
Arturo Guzmán Lucena <arturo.s238739@cesurformacion.com>
2023-04-12 23:32:33 +0200

    Avance Electron

--- src/main/Electron/main.js
+++ src/main/Electron/main.js
@@ -1,21 +1,32 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const mousetrap = require('mousetrap');
+const path = require('path');
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        icon:'Drawables/GeneralImages/RiftStatisticsOnlyLogo.png',
-        width: 1200,
-        minWidth: 1200,
-        height: 1000,
-        minHeight: 1000
+    // Configurar electron-reload para observar cambios en la carpeta.
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    hardResetMethod: 'exit'
+});
+
+
+    const createWindow = () => {
+        const win = new BrowserWindow({
+            icon:'media/logo/RiftStatisticsOnlyLogo.png',
+            width: 1200,
+            minWidth: 1200,
+            height: 1000,
+            minHeight: 1000
+        })
+
+        win.removeMenu()
+        win.openDevTools();
+        win.loadFile('index.html')
+    }
+
+    app.whenReady().then(() => {
+        createWindow()
     })
-    win.removeMenu()
-    win.loadFile('index.html')
-}
 
-app.whenReady().then(() => {
-    createWindow()
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit()
+    })
