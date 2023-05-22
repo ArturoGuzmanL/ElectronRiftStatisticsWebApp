42b455d8eebd10bc9347e65c527dbcaa9e1c267a
Arturo Guzmán Lucena <arturo.s238739@cesurformacion.com>
2023-05-22 14:48:19 +0200

    Finalizacion de webApp

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
 const {app, BrowserWindow, session,ipcMain, net} = require("electron");
-const { protocol } = require('electron');
 const fs = require("fs");
 const path = require('path');
-const {format} = require("util");
-const {existsSync} = require("next/dist/lib/find-pages-dir");
 const SHA256 = require("crypto-js/sha256");
-const {request} = require("net");
-const {wait} = require("next/dist/build/output/log");
 global.remote = require('electron').remote;
-const Swal = require("sweetalert2");
-const Swaldef = Swal.default;
-const axios = require('axios');
-const mousetrap = require('mousetrap');
-const ChildProcess = require("child_process");
-const {networkInterfaces} = require("os");
-
-if (handleSquirrelEvent(app)) {
-  return;
-}
-
-// require('electron-reload')(__dirname, {
-//   electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-//   hardResetMethod: 'exit'
-// });
 
 ipcMain.on("reload", (event) => {
   mainWindow.reload();
@@ -144,8 +124,8 @@ async function createWindow() {
   mainWindow = new BrowserWindow({
     icon: 'assets/icons/Linux.png ',
     width: 1200,
-    minWidth: 1000,
-    height: 1000,
+    minWidth: 1016,
+    height: 1016,
     backgroundColor: '#0a0c23',
     minHeight: 800,
     webPreferences: {
@@ -158,8 +138,6 @@ async function createWindow() {
   mainWindow.removeMenu()
   mainWindow.openDevTools();
 
-  let pathTF = path.join(__dirname, 'Pruebashtml', 'unloggedAccountSettings.html');
-  // mainWindow.loadFile(pathTF);
   AccountinfoExists();
   requestInitialPage();
 
@@ -302,94 +280,31 @@ async function requestInitialPage() {
                   mainWindow.loadURL(`https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/true/${uidA}`);
                 } else {
                   mainWindow.loadURL("https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/false/null");
-                  return;
+
                 }
               });
               netRequest.end();
             } else {
               mainWindow.loadURL("https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/false/null");
-              return;
+
             }
           } else {
             mainWindow.loadURL("https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/false/null");
-            return;
+
           }
         } else {
           mainWindow.loadURL("https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/false/null");
-          return;
+
         }
       } else {
         mainWindow.loadURL("https://riftstatistics.ddns.net/page/htmlRequests/home/initialization/false/null");
-        return;
+
       }
 
     });
   }
 }
 
-function handleSquirrelEvent(application) {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function(command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, {
-        detached: true
-      });
-    } catch (error) {}
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function(args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(application.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(application.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      application.quit();
-      return true;
-  }
-}
 function getUserId() {
   let pathToUserData = app.getPath('userData');
   let pathToFile = path.join(pathToUserData, 'Session', 'Session.txt');
